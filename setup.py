c96e30543670d9284f3647c822e28d06dacd3b8a
Aurelien BOMPARD <aurelien.bompard@c-s.fr>
2009-09-22 13:30:25 +0000

    Chaque module est indépendant d'un point de vue construction python. Le makefile et le buildout.cfg étendent les fichiers génériques dans glue.
    Il doit suffire de taper make pour construire le paquet. Les commandes
    "make tests", "make lint" et "make apidoc" continuent à fonctionner.
    
    
    git-svn-id: https://vigilo-dev.si.c-s.fr/svn@746 b22e2e97-25c9-44ff-b637-2e5ceca36478

--- setup.py
+++ setup.py
@@ -15,6 +15,7 @@ setup(name='vigilo-connector-nagios',
         install_requires=[
             # dashes become underscores
             # order is important (wokkel before Twisted)
+            'setuptools',
             'coverage',
             'nose',
             'pylint',
