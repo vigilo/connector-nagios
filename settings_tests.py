626ca204e4b274c34d60d3575d45bc88109151cb
Serge MOIGNARD <serge.moignard@c-s.fr>
2009-09-29 13:59:52 +0000

    version départ connector-nagios
    git-svn-id: https://vigilo-dev.si.c-s.fr/svn@780 b22e2e97-25c9-44ff-b637-2e5ceca36478

---
+++ settings_tests.py
@@ -0,0 +1,21 @@
+import logging
+LOGGING_PLUGINS = ()
+LOGGING_SETTINGS = { 'level': logging.DEBUG, }
+LOGGING_LEVELS = {}
+LOGGING_SYSLOG = False
+
+
+
+VIGILO_CONNECTOR_DAEMONIZE = True
+VIGILO_CONNECTOR_PIDFILE = '/home/smoignar/var/vigilo/connector-nagios/connector-nagios.pid'
+VIGILO_CONNECTOR_XMPP_SERVER_HOST = 'localhost'
+VIGILO_CONNECTOR_XMPP_PUBSUB_SERVICE = 'pubsub.localhost'
+# Respect the ejabberd namespacing, for now. It will be too restrictive soon.
+VIGILO_CONNECTOR_JID = 'user-nagios@localhost'
+VIGILO_CONNECTOR_PASS = 'user-nagios'
+
+#VIGILO_CONNECTOR_TOPIC = '/home/tburguie3/connectorx/BUS'
+VIGILO_SOCKETR = '/var/lib/vigilo/connector-nagios/send.sock'
+VIGILO_MESSAGE_BACKUP_FILE = '/var/lib/vigilo/connector-nagios/backup'
+VIGILO_MESSAGE_BACKUP_TABLE_TOBUS = 'connector_tobus'
+
---
+++ tests/test_connector_nagios.py
@@ -0,0 +1,31 @@
+'''
+Created on 29 sept. 2009
+@author: smoignar
+
+'''
+from __future__ import absolute_import
+
+import unittest
+
+from vigilo.connector.converttoxml import text2xml
+
+NS_EVENT = 'http://www.projet-vigilo.org/xmlns/event1'
+NS_PERF = 'http://www.projet-vigilo.org/xmlns/perf1'
+
+class TestSequenceFunctions(unittest.TestCase):
+    """ Test the connector nagios functions """
+    
+    def test_event2xml(self):
+        """ Test the connector function event2xml """
+        dico = {'ns': NS_EVENT}
+        # subfunction event2xml 
+        self.assertEqual("""<event xmlns='%(ns)s'><timestamp>1165939739</timestamp><host>serveur1.example.com</host><ip>192.168.0.1</ip><service>Load</service><state>CRITICAL</state><message>CRITICAL: load avg: 12 10 10</message></event>""" % dico, text2xml("""event|1165939739|serveur1.example.com|192.168.0.1|Load|CRITICAL|CRITICAL: load avg: 12 10 10""").toXml())
+    
+    def test_perft2xml(self):
+        """ Test the connector function perf2xml """
+        dico = {'ns': NS_PERF}
+        # subfunction perf2xml 
+        self.assertEqual("""<perf xmlns='%(ns)s'><timestamp>1165939739</timestamp><host>serveur1.example.com</host><datasource>Load</datasource><value>10</value></perf>""" % dico, text2xml("""perf|1165939739|serveur1.example.com|Load|10""").toXml())   
+    
+if __name__ == "__main__": 
+    unittest.main()
