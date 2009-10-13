66474167c238111d437d03ccebbacf10ecd9fdcd
Thomas BURGUIERE <thomas.burguiere@c-s.fr>
2009-10-13 14:09:01 +0000

    tburguie: ajout de l'écriture dans le pipe Nagios
    git-svn-id: https://vigilo-dev.si.c-s.fr/svn@877 b22e2e97-25c9-44ff-b637-2e5ceca36478

---
+++ src/vigilo/connector_nagios/main.py
@@ -0,0 +1,114 @@
+# vim: set fileencoding=utf-8 sw=4 ts=4 et :
+""" Nagios connector Pubsub client. """
+from __future__ import absolute_import, with_statement
+
+from twisted.application import app, service
+from twisted.internet import reactor
+from twisted.words.protocols.jabber.jid import JID
+
+
+from wokkel import client
+from vigilo.common.gettext import translate
+_ = translate(__name__)
+
+class ConnectorServiceMaker(object):
+    """
+    Creates a service that wraps everything the connector needs.
+    """
+
+    #implements(service.IServiceMaker, IPlugin)
+
+    def makeService(self):
+        """ the service that wraps everything the connector needs. """ 
+        from vigilo.connector_nagios.xmpptopipefw import XMPPToPipeForwarder
+        from vigilo.connector.sockettonodefw import SocketToNodeForwarder
+        from vigilo.pubsub.checknode import VerificationNode
+        from vigilo.common.conf import settings
+        from vigilo.common.logging import get_logger
+        import os
+        LOGGER = get_logger(__name__)
+
+        xmpp_client = client.XMPPClient(
+                JID(settings['VIGILO_CONNECTOR_JID']),
+                settings['VIGILO_CONNECTOR_PASS'],
+                settings['VIGILO_CONNECTOR_XMPP_SERVER_HOST'])
+        xmpp_client.logTraffic = True
+        xmpp_client.setName('xmpp_client')
+
+        list_nodeOwner = settings.get('VIGILO_CONNECTOR_TOPIC_OWNER', [])
+        list_nodeSubscriber = settings.get('VIGILO_CONNECTOR_TOPIC', [])
+        verifyNode = VerificationNode(list_nodeOwner, list_nodeSubscriber, 
+                                      doThings=True)
+        verifyNode.setHandlerParent(xmpp_client)
+        nodetopublish = settings.get('VIGILO_CONNECTOR_TOPIC_PUBLISHER', None)
+        _service = JID(settings.get('VIGILO_CONNECTOR_XMPP_PUBSUB_SERVICE',
+                                    None))
+
+        bkpfile = settings.get('VIGILO_MESSAGE_BACKUP_FILE', ":memory:")
+        pw = settings.get('VIGILO_PIPEW', None)
+        sr = settings.get('VIGILO_SOCKETR', None)
+
+        for i in bkpfile, pw, sr:
+            if i != ':memory:' and i is not None:
+                if not os.access(os.path.dirname(i), os.F_OK):
+                    msg = _("Directory not found: '%(dir)s'") % \
+                            {'dir': os.path.dirname(i)}
+                    LOGGER.error(msg)
+                    raise OSError(msg)
+                if not os.access(os.path.dirname(i), os.R_OK):
+                    msg = _("Directory not readable: '%(dir)s'") % \
+                            {'dir': os.path.dirname(i)}
+                    LOGGER.error(msg)
+                    raise OSError(msg)
+                if not os.access(os.path.dirname(i), os.W_OK):
+                    msg = _("Directory not writable: '%(dir)s'") % \
+                            {'dir': os.path.dirname(i)}
+                    LOGGER.error(msg)
+                    raise OSError(msg)
+                if not os.access(os.path.dirname(i), os.X_OK):
+                    msg = _("Directory not executable: '%(dir)s'") % \
+                            {'dir': os.path.dirname(i)}
+                    LOGGER.error(msg)
+                    raise OSError(msg)
+
+        if pw is not None:
+            message_consumer = XMPPToPipeForwarder(
+                    pw,
+                    bkpfile,
+                    settings['VIGILO_MESSAGE_BACKUP_TABLE_FROMBUS'])
+            message_consumer.setHandlerParent(xmpp_client)
+
+
+        if sr is not None:
+            message_publisher = SocketToNodeForwarder(
+                    sr,
+                    bkpfile,
+                    settings['VIGILO_MESSAGE_BACKUP_TABLE_TOBUS'],
+                    nodetopublish,
+                    _service)
+            message_publisher.setHandlerParent(xmpp_client)
+
+        root_service = service.MultiService()
+        xmpp_client.setServiceParent(root_service)
+        return root_service
+
+def do_main_programm():
+    """ main function designed to launch the program """
+    application = service.Application('Twisted PubSub component')
+    conn_service = ConnectorServiceMaker().makeService()
+    conn_service.setServiceParent(application)
+    app.startApplication(application, False)
+    reactor.run()
+
+def main():
+    """ main function designed to launch the program """
+
+    from vigilo.common.daemonize import daemonize
+    context = daemonize()
+    with context:
+        do_main_programm()
+
+
+if __name__ == '__main__':
+    main()
+
---
+++ src/vigilo/connector_nagios/xmpptopipefw.py
@@ -0,0 +1,144 @@
+# vim: set fileencoding=utf-8 sw=4 ts=4 et :
+
+"""
+Extends pubsub clients to compute Node message.
+"""
+from __future__ import absolute_import
+
+
+from vigilo.common.logging import get_logger
+from vigilo.connector.store import DbRetry
+import os
+import stat
+from wokkel.subprotocols import XMPPHandler
+from wokkel import xmppim
+
+LOGGER = get_logger(__name__)
+
+from vigilo.common.gettext import translate
+_ = translate(__name__)
+
+class XMPPToPipeForwarder(XMPPHandler):
+    """
+    Receives messages from xmpp, and passes them to the pipe.
+    Forward XMPP to pipe.
+    """
+    def connectionInitialized(self):
+        """
+        redefinition in order to add :
+            - new observer for message type=chat;
+            - sending presence.
+
+        """
+        # Called when we are connected and authenticated
+        XMPPHandler.connectionInitialized(self)
+        # add an observer to deal with chat message (oneToOne message)
+        self.xmlstream.addObserver("/message[@type='chat']", self.chatReceived)
+
+        
+        # There's probably a way to configure it (on_sub vs on_sub_and_presence)
+        # but the spec defaults to not sending subscriptions without presence.
+        self.send(xmppim.AvailablePresence())
+        LOGGER.info(_('ConnectionInitialized'))
+
+
+
+    def __init__(self, pipe_filename, dbfilename, dbtable):
+        XMPPHandler.__init__(self)
+        self.retry = DbRetry(dbfilename, dbtable)
+        self.__backuptoempty = os.path.exists(dbfilename)
+        self.pipe_filename = pipe_filename
+        self.__emptyingbackup = False
+
+    def sendQueuedMessages(self):
+        """
+        Called to send Message previously stored
+        """
+        if self.__emptyingbackup: 
+            return
+        self.__emptyingbackup = True
+        if self.__backuptoempty:
+            # XXX Ce code peut potentiellement boucler indéfiniment...
+            while True:
+                msg = self.retry.unstore()
+                if msg is None:
+                    break
+                else:
+                    if self.messageForward(msg) is not True:
+                        break
+            self.__backuptoempty = False
+            self.retry.vacuum()
+        self.__emptyingbackup = False
+
+
+    def connectionMade(self):
+        """Called when a connection is made.
+
+        This may be considered the initializer of the protocol, because
+        it is called when the connection is completed.  For clients,
+        this is called once the connection to the server has been
+        established; for servers, this is called after an accept() call
+        stops blocking and a socket has been received.  If you need to
+        send any greeting or initial message, do it here.
+        """
+        self.sendQueuedMessages()
+    
+    def messageForward(self, msg):
+        """
+        function to forward the message to the pipe
+        @param msg: message to forward
+        @type msg: C{str}
+        """
+
+        if self.__backuptoempty and not self.__emptyingbackup:
+            self.sendQueuedMessages()
+        try:
+            # testing there is a pipe (FIFO) which exist.
+            if not stat.S_ISFIFO(os.stat(self.pipe_filename).st_mode):
+                self.retry.store(msg)
+                self.__backuptoempty = True
+                return
+            # XXX il serait possible d'aggréger les écritures si on flush
+            #  après chaque écriture (pour ne pas ouvrir/fermer le pipe à
+            # chaque fois
+            # (texte suivant issue du code de nsca)
+# /* if we don't fflush() then we're writing in 4k non-CR-terminated blocks, and
+#  * anything else (eg. pscwatch) which writes to the file will be writing into
+#  * the middle of our commands.
+#  */
+            pipe = open(self.pipe_filename, 'w')
+            pipe.write(msg + '\n')
+            pipe.close()
+            return True
+        except OSError, e:
+            LOGGER.error(_('Message impossible to forward %(error_message)s' +
+                           ', the message is stored for later reemission') % \
+                            {'error_message': e.__str__()})
+            self.retry.store(msg)
+            self.__backuptoempty = True
+
+
+    def chatReceived(self, msg):
+        """ 
+        function to treat a received chat message 
+        
+        @param msg: msg to treat
+        @type  msg: Xml object
+
+        """
+        # It should only be one body
+        # Il ne devrait y avoir qu'un seul corps de message (body)
+        bodys = [element for element in msg.elements()
+                         if element.name in ('body',)]
+
+        for b in bodys:
+            # the data we need is just underneath
+            # les données dont on a besoin sont juste en dessous
+            #self.messageForward(b.toXml().encode('utf8'))
+            #return
+            for data in b.elements():
+                LOGGER.debug(_('Message from chat message to forward: ' +
+                               '%s') %
+                               data.toXml().encode('utf8'))
+                self.messageForward(data.toXml().encode('utf8'))
+
