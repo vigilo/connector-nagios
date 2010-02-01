085c5b1141a0f56d10b698a040a206cae8dcb8b3
Francois POIROTTE <francois.poirotte@c-s.fr>
2010-02-01 16:17:13 +0000

    Ajout d'un script permettant de simuler l'arrivée d'alertes issues de Nagios. Utilisation :   Lancer un connecteur générique (cd connector && bin/connector)   Dans une autre console, lancer le simulateur :     ./simulateur_event.sh | socat - UNIX-CONNECT:/var/lib/vigilo/connector/send.sock   Facultatif :     Dans une nouvelle console, lancer un corrélateur (cd ../correlator && bin/correlator)     Dans une nouvelle console, lancer Vigiboard (cd ../vigiboard && bin/paster serve development.ini)     Observer les résultats dans les différentes consoles et sur Vigiboard.
    git-svn-id: https://vigilo-dev.si.c-s.fr/svn@1815 b22e2e97-25c9-44ff-b637-2e5ceca36478

---
+++ simulateur_event.sh
@@ -0,0 +1,73 @@
+#!/bin/bash
+if [[ "$1" == "--help" || "$1" == "-h" ]] ; then
+    exit 0
+fi
+
+# les états nagios
+STATE=(CRITICAL WARNING UNKNOWN OK OK)
+# les services supervisés
+# L'entrée '' permet d'envoyer des alertes sur les hôtes directement.
+INDICATEUR=('Interface eth0' 'Interface eth1' 'Interface eth2' '')
+# la liste des serveurs
+# La valeur 'HLS' est réservée et correspond
+# aux services de haut niveau (High-Level Services).
+SERVER=(host2.example.com)
+# les délais entre 2 messages (min et max)
+DELAI_MIN=3
+DELAI_MAX=5
+
+
+
+function random() {
+    rand=$RANDOM
+    MIN=$1
+    MAX=$2
+    MAX=$(($MAX + 1))
+    let "rand %= $MAX"
+    while [ "$rand" -lt $MIN ]; do 
+        rand=$RANDOM
+        let "rand %= $MAX"  # Scales $rand down within $MAX.
+    done
+    echo $rand
+}
+
+function random_wait() {
+    seconde_waited=`random 10 20`
+    sleep $seconde_waited
+}
+
+function random_state () {
+    min=0
+    max=${#STATE[@]}
+    max=$(($max - 1))
+    random=`random $min $max`
+    echo -n ${STATE[$random]}
+}
+
+function random_indicateur () {
+    min=0
+    max=${#INDICATEUR[@]}
+    max=$(($max - 1))
+    random=`random $min $max`
+    echo -n ${INDICATEUR[$random]}
+}
+
+function random_serveur () {
+    min=0
+    max=${#SERVER[@]}
+    max=$(($max - 1))
+    random=`random $min $max`
+    echo -n ${SERVER[$random]}
+}
+
+while true; do 
+    seconde=`date +%s`
+    value=`random 0 100`
+    ind=`random_indicateur`
+    ser=`random_serveur`
+    ser_name=`echo $ser | awk -F" " '{ print $1 }'`
+    sta=`random_state`
+    echo "event|$seconde|$ser_name|$ind|$sta|$sta:  $ind:  $value  $value  $value"
+    sleep `random $DELAI_MIN $DELAI_MAX`
+done
+
