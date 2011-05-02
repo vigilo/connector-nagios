361773e829fcb96dbe51fe430619bbb9a3c11d98
Aurelien BOMPARD <aurelien.bompard@c-s.fr>
2011-05-02 15:37:49 +0000

    connector-nagios: simplification des options et gestion de la présence
    Les deux sens du connecteur sont maintenant obligatoires (depuis et vers
    Nagios), pour simplifier un peu les options. Ça permet aussi d'ajouter la
    gestion de la présence à la partie Bus vers Nagios, qui respectera donc le
    `max_queue_size`.
    
    git-svn-id: https://vigilo-dev.si.c-s.fr/svn@6954 b22e2e97-25c9-44ff-b637-2e5ceca36478

--- src/twisted/plugins/vigilo_nagios.py
+++ src/twisted/plugins/vigilo_nagios.py
@@ -3,8 +3,9 @@
 # License: GNU GPL v2 <http://www.gnu.org/licenses/gpl-2.0.html>
 
 """Nagios <-> XMPP connector"""
-from __future__ import absolute_import, with_statement
+from __future__ import absolute_import
 import os
+import sys
 
 from zope.interface import implements
 from twisted.plugin import IPlugin
@@ -46,8 +47,12 @@ class NagiosConnectorServiceMaker(object):
         _service = JID(settings['bus'].get('service', None))
 
         bkpfile = settings['connector'].get('backup_file', ":memory:")
-        pw = settings['connector-nagios'].get('nagios_pipe', None)
-        sr = settings['connector-nagios'].get('listen_unix', None)
+        try:
+            pw = settings['connector-nagios']['nagios_pipe']
+            sr = settings['connector-nagios']['listen_unix']
+        except KeyError, e:
+            LOGGER.error(_("Missing configuration option: %s"), str(e))
+            sys.exit(1)
 
         if bkpfile != ':memory:':
             if not os.path.exists(os.path.dirname(bkpfile)):
@@ -61,45 +66,41 @@ class NagiosConnectorServiceMaker(object):
                 LOGGER.error(msg)
                 raise OSError(msg)
 
-        if sr:
-            if not os.path.exists(os.path.dirname(sr)):
-                msg = _("Directory not found: '%(dir)s'") % \
-                        {'dir': os.path.dirname(sr)}
-                LOGGER.error(msg)
-                raise OSError(msg)
-            if not os.access(os.path.dirname(sr), os.R_OK | os.W_OK | os.X_OK):
-                msg = _("Wrong permissions on directory: '%(dir)s'") % \
-                        {'dir': os.path.dirname(sr)}
-                LOGGER.error(msg)
-                raise OSError(msg)
+        # De Nagios vers le bus
+        if not os.path.exists(os.path.dirname(sr)):
+            msg = _("Directory not found: '%(dir)s'") % \
+                    {'dir': os.path.dirname(sr)}
+            LOGGER.error(msg)
+            raise OSError(msg)
+        if not os.access(os.path.dirname(sr), os.R_OK | os.W_OK | os.X_OK):
+            msg = _("Wrong permissions on directory: '%(dir)s'") % \
+                    {'dir': os.path.dirname(sr)}
+            LOGGER.error(msg)
+            raise OSError(msg)
+        message_publisher = SocketToNodeForwarder(
+                sr, bkpfile,
+                settings['connector']['backup_table_to_bus'])
+        message_publisher.setHandlerParent(xmpp_client)
 
-        if pw:
-            if os.path.exists(pw) and not os.access(pw, os.W_OK):
-                msg = _("Can't write to the Nagios command pipe: '%s'") % pw
-                LOGGER.error(msg)
-                raise OSError(msg)
-            if not os.access(os.path.dirname(pw), os.X_OK):
-                msg = _("Can't traverse directory: '%(dir)s'") % \
-                        {'dir': os.path.dirname(pw)}
-                LOGGER.error(msg)
-                raise OSError(msg)
-
-            message_consumer = XMPPToPipeForwarder(
-                    pw,
-                    bkpfile,
-                    settings['connector']['backup_table_from_bus'])
-            message_consumer.setHandlerParent(xmpp_client)
-
-
-        if sr is not None:
-            message_publisher = SocketToNodeForwarder(
-                    sr, bkpfile,
-                    settings['connector']['backup_table_to_bus'])
-            message_publisher.setHandlerParent(xmpp_client)
+        # Du bus vers Nagios
+        if os.path.exists(pw) and not os.access(pw, os.W_OK):
+            msg = _("Can't write to the Nagios command pipe: '%s'") % pw
+            LOGGER.error(msg)
+            raise OSError(msg)
+        if not os.access(os.path.dirname(pw), os.X_OK):
+            msg = _("Can't traverse directory: '%(dir)s'") % \
+                    {'dir': os.path.dirname(pw)}
+            LOGGER.error(msg)
+            raise OSError(msg)
+        message_consumer = XMPPToPipeForwarder(
+                pw,
+                bkpfile,
+                settings['connector']['backup_table_from_bus'])
+        message_consumer.setHandlerParent(xmpp_client)
 
         # Présence
         from vigilo.connector.presence import PresenceManager
-        presence_manager = PresenceManager()
+        presence_manager = PresenceManager(message_consumer)
         presence_manager.setHandlerParent(xmpp_client)
 
         # Statistiques
