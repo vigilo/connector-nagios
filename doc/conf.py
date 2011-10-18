33213791c595854ca1aaf171b7c995bae1419e28
Aurelien BOMPARD <aurelien.bompard@c-s.fr>
2011-10-18 16:31:52 +0200

    Activation de la doc PDF / Latex
    Change-Id: I135d28e54a6b21e39c2ccd9637b3caa16756ed3d

--- doc/conf.py
+++ doc/conf.py
@@ -3,8 +3,15 @@
 project = u'Vigilo connector-nagios'
 
 pdf_documents = [
-        ('admin', "connector-nagios-admin", "Connector-nagios : Guide d'administration", u'Vigilo'),
-        ('dev', "connector-nagios-dev", "Connector-nagios : Guide de développement", u'Vigilo'),
+        ('admin', "admin-connector-nagios", "Connector-nagios : Guide d'administration", u'Vigilo'),
+        ('dev', "dev-connector-nagios", "Connector-nagios : Guide de développement", u'Vigilo'),
+]
+
+latex_documents = [
+        ('admin', 'admin-connector-nagios.tex', u"Connector-nagios : Guide d'administration",
+         'AA100004-2/ADM00002', 'vigilo'),
+        ('dev', 'dev-connector-nagios.tex', u"Connector-nagios : Guide de développement",
+         'AA100004-2/DEV00006', 'vigilo'),
 ]
 
 execfile("../buildenv/doc/conf.py")
