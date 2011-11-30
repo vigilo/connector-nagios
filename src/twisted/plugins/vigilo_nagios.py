84831a7a900cae79252ced82224e11de3a976635
Aurelien BOMPARD <aurelien.bompard@c-s.fr>
2011-11-30 14:18:49 +0100

    Connecteur Nagios
    Change-Id: Iefc176f56e3626acd3ee7ede4495a6f89e0bb7e2

--- src/twisted/plugins/vigilo_nagios.py
+++ src/twisted/plugins/vigilo_nagios.py
@@ -4,19 +4,12 @@
 
 """Nagios <-> XMPP connector"""
 from __future__ import absolute_import
-import os
-import sys
 
 from zope.interface import implements
 from twisted.plugin import IPlugin
 from twisted.application import service
-from twisted.words.protocols.jabber.jid import JID
 
-from vigilo.common.gettext import translate
-from vigilo.connector import client
 from vigilo.connector import options as base_options
-
-_ = translate('vigilo.connector_nagios')
 
 class NagiosConnectorServiceMaker(object):
     """
@@ -29,97 +22,7 @@ class NagiosConnectorServiceMaker(object):
 
     def makeService(self, options):
         """ the service that wraps everything the connector needs. """
-        from vigilo.common.conf import settings
-        if options["config"] is not None:
-            settings.load_file(options["config"])
-        else:
-            settings.load_module('vigilo.connector_nagios')
-
-        from vigilo.common.logging import get_logger
-        LOGGER = get_logger('vigilo.connector_nagios')
-
-        from vigilo.connector_nagios.xmpptopipefw import XMPPToPipeForwarder
-        from vigilo.connector_nagios.nagiossender import NagiosSender
-
-        xmpp_client = client.client_factory(settings)
-
-        nodetopublish = settings.get('publications', {})
-        _service = JID(settings['bus'].get('service', None))
-
-        bkpfile = settings['connector'].get('backup_file', ":memory:")
-        try:
-            pw = settings['connector-nagios']['nagios_pipe']
-            sr = settings['connector-nagios']['listen_unix']
-        except KeyError, e:
-            LOGGER.error(_("Missing configuration option: %s"), str(e))
-            sys.exit(1)
-
-        if bkpfile != ':memory:':
-            if not os.path.exists(os.path.dirname(bkpfile)):
-                msg = _("Directory not found: '%(dir)s'") % \
-                        {'dir': os.path.dirname(bkpfile)}
-                LOGGER.error(msg)
-                raise OSError(msg)
-            if not os.access(os.path.dirname(bkpfile), os.R_OK | os.W_OK | os.X_OK):
-                msg = _("Wrong permissions on directory: '%(dir)s'") % \
-                        {'dir': os.path.dirname(bkpfile)}
-                LOGGER.error(msg)
-                raise OSError(msg)
-
-        # De Nagios vers le bus
-        if not os.path.exists(os.path.dirname(sr)):
-            msg = _("Directory not found: '%(dir)s'") % \
-                    {'dir': os.path.dirname(sr)}
-            LOGGER.error(msg)
-            raise OSError(msg)
-        if not os.access(os.path.dirname(sr), os.R_OK | os.W_OK | os.X_OK):
-            msg = _("Wrong permissions on directory: '%(dir)s'") % \
-                    {'dir': os.path.dirname(sr)}
-            LOGGER.error(msg)
-            raise OSError(msg)
-        message_publisher = NagiosSender(
-                sr, bkpfile,
-                settings['connector']['backup_table_to_bus'])
-        message_publisher.setHandlerParent(xmpp_client)
-
-        # Du bus vers Nagios
-        if os.path.exists(pw) and not os.access(pw, os.W_OK):
-            msg = _("Can't write to the Nagios command pipe: '%s'") % pw
-            LOGGER.error(msg)
-            raise OSError(msg)
-        if not os.access(os.path.dirname(pw), os.X_OK):
-            msg = _("Can't traverse directory: '%(dir)s'") % \
-                    {'dir': os.path.dirname(pw)}
-            LOGGER.error(msg)
-            raise OSError(msg)
-        message_consumer = XMPPToPipeForwarder(
-                pw,
-                bkpfile,
-                settings['connector']['backup_table_from_bus'])
-        message_consumer.setHandlerParent(xmpp_client)
-        # pour les stats: gestion de la file d'attente du consommateur
-        message_publisher.from_bus = message_consumer
-
-        # Présence
-        from vigilo.connector.presence import PresenceManager
-        presence_manager = PresenceManager()
-        presence_manager.setHandlerParent(xmpp_client)
-        message_consumer.registerProducer(presence_manager, True)
-
-        # Statistiques
-        from vigilo.connector.status import StatusPublisher
-        servicename = options["name"]
-        if servicename is None:
-            servicename = "vigilo-connector-nagios"
-        stats_publisher = StatusPublisher(message_publisher,
-                        settings["connector"].get("hostname", None),
-                        servicename=servicename,
-                        node=settings["connector"].get("status_node", None))
-        stats_publisher.setHandlerParent(xmpp_client)
-        presence_manager.registerStatusPublisher(stats_publisher)
-
-        root_service = service.MultiService()
-        xmpp_client.setServiceParent(root_service)
-        return root_service
+        from vigilo.connector_nagios import makeService
+        return makeService(options)
 
 nagios_connector = NagiosConnectorServiceMaker()
--- src/vigilo/connector_nagios/__init__.py
+++ src/vigilo/connector_nagios/__init__.py
@@ -1,1 +1,80 @@
-""" vigilo connector nagios """
+# vim: set fileencoding=utf-8 sw=4 ts=4 et :
+# Copyright (C) 2006-2011 CS-SI
+# License: GNU GPL v2 <http://www.gnu.org/licenses/gpl-2.0.html>
+
+"""Nagios <-> XMPP connector"""
+
+
+from __future__ import absolute_import
+import os
+import sys
+
+from twisted.application import service
+
+
+def makeService(options):
+    """ the service that wraps everything the connector needs. """
+    from vigilo.connector import getSettings
+    settings = getSettings(options)
+
+    from vigilo.common.logging import get_logger
+    LOGGER = get_logger(__name__)
+
+    from vigilo.common.gettext import translate
+    _ = translate(__name__)
+
+    from vigilo.connector.client import client_factory
+    from vigilo.connector.forwarder import buspublisher_factory
+    from vigilo.connector.forwarder import backupprovider_factory
+    from vigilo.connector.socket import socketlistener_factory
+    from vigilo.connector.status import statuspublisher_factory
+    #from vigilo.connector_nagios.xmpptopipefw import XMPPToPipeForwarder
+    #from vigilo.connector_nagios.nagiossender import NagiosSender
+
+    try:
+        pw = settings['connector-nagios']['nagios_pipe']
+        socket_filename = settings['connector-nagios']['listen_unix']
+    except KeyError, e:
+        LOGGER.error(_("Missing configuration option: %s"), str(e))
+        sys.exit(1)
+
+    root_service = service.MultiService()
+
+    client = client_factory(settings)
+    client.setServiceParent(root_service)
+
+    bus_publisher = buspublisher_factory(settings, client)
+    socket_listener = socketlistener_factory(socket_filename)
+
+    backup_provider = backupprovider_factory(settings, socket_listener)
+    backup_provider.stat_names["queue"] = "queue-from-nagios"
+    backup_provider.setServiceParent(root_service)
+
+    bus_publisher.registerProducer(backup_provider, True)
+
+    # Statistiques
+    servicename = options["name"]
+    if servicename is None:
+        servicename = "vigilo-connector-nagios"
+    status_publisher = statuspublisher_factory(settings, servicename, client,
+            providers=[bus_publisher, backup_provider])
+
+    ## Du bus vers Nagios
+    #if os.path.exists(pw) and not os.access(pw, os.W_OK):
+    #    msg = _("Can't write to the Nagios command pipe: '%s'") % pw
+    #    LOGGER.error(msg)
+    #    raise OSError(msg)
+    #if not os.access(os.path.dirname(pw), os.X_OK):
+    #    msg = _("Can't traverse directory: '%(dir)s'") % \
+    #            {'dir': os.path.dirname(pw)}
+    #    LOGGER.error(msg)
+    #    raise OSError(msg)
+    #message_consumer = XMPPToPipeForwarder(
+    #        pw,
+    #        bkpfile,
+    #        settings['connector']['backup_table_from_bus'])
+    #message_consumer.setHandlerParent(xmpp_client)
+    ## pour les stats: gestion de la file d'attente du consommateur
+    #message_publisher.from_bus = message_consumer
+
+    return root_service
